14e3b1006b51783125c39b4c7ce06d3e5242623a
aquito <aki@mygamestudies.com>
2019-09-07 17:57:36 +0100

    Initial commit

---
+++ Assets/Scripts/MeshHighlighter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+public class MeshHighlighter : MonoBehaviour
+{
+    private MeshRenderer originalMesh;
+    [SerializeField]
+    private MeshRenderer highlightedMesh;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalMesh = GetComponent<MeshRenderer>(); 
+    }
+
+    public void EnableHighlight(bool onOff)
+    {
+        if(highlightedMesh != null)
+        {
+            highlightedMesh.enabled = onOff;
+            originalMesh.enabled = !onOff;
+        }
+    }
+
+    /// <summary>
+    /// Called when the mouse enters the GUIElement or Collider.
+    /// </summary>
+    void OnMouseEnter()
+    {
+        EnableHighlight(true);
+    }
+
+    /// <summary>
+    /// Called when the mouse is not any longer over the GUIElement or Collider.
+    /// </summary>
+    void OnMouseExit()
+    {
+        EnableHighlight(false);
+    }
+}
